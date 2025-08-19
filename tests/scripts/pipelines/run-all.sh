2d458374ebef6a13f8013d05d5056bcd626a16f3
karsten-klein <karsten.klein@metaeffekt.com>
2025-08-19 18:44:19 +0200

    AE-1277: synchronizing with customer project

--- tests/scripts/pipelines/run-all.sh
+++ tests/scripts/pipelines/run-all.sh
@@ -23,26 +23,26 @@ set -euo pipefail
 
 SELF_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_PATH="$SELF_DIR/../config.sh"
-PROCESSOR_SPECIFIC_SCRIPTS_DIR="$SELF_DIR/../processor-specific"
+PROCESSOR_SCRIPTS_DIR="$SELF_DIR/../processors"
 
 # Script Pipeline
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/util_create-diff.sh"
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/util_merge-inventories.sh"
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/util_transform-inventories.sh"
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/util_update-mirror.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/util_create-diff.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/util_merge-inventories.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/util_transform-inventories.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/util_update-mirror.sh"
 
 # requires service
-# sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/util_portfolio-transfer.sh"
+# sh "$PROCESSOR_SCRIPTS_DIR/util_portfolio-transfer.sh"
 
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/util_aggregate-sources.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/util_aggregate-sources.sh"
 
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/analyze_resolve-inventory.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/analyze_resolve-inventory.sh"
 
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/convert_inventory-to-cyclonedx.sh"
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/convert_inventory-to-spdx.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/convert_inventory-to-cyclonedx.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/convert_inventory-to-spdx.sh"
 
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/advise_attach-metadata.sh"
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/advise_enrich-inventory.sh"
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/advise_create-dashboard.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/advise_attach-metadata.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/advise_enrich-inventory.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/advise_create-dashboard.sh"
 
-sh "$PROCESSOR_SPECIFIC_SCRIPTS_DIR/scan_inventory-scanner.sh"
+sh "$PROCESSOR_SCRIPTS_DIR/scan_inventory-scanner.sh"
