c73aad871174014aac2f802651708e18b1bee2ab
karsten-klein <karsten.klein@metaeffekt.com>
2025-10-10 17:12:28 +0200

    AE-1434: provide job for jenkins

--- tests/scripts/pipelines/run_workspace-001.sh
+++ tests/scripts/pipelines/run_workspace-001.sh
@@ -9,7 +9,7 @@ logger_init "INFO" "$SELF_DIR/../../../.logs/run_all.log" "true"
 
 LOG_FILE="$SELF_DIR/../../../.logs/$(basename $0).log"
 export LOG_FILE
-export LOG_LEVEL="INFO"
+export LOG_LEVEL="ALL"
 
 bash "$SELF_DIR/run_util.sh"
 bash "$SELF_DIR/run_prepare.sh"
