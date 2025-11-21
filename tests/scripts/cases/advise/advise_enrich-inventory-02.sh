cd1f5844d744a558adc913ea468c2ff4e0dd03cb
rmtrms <91293797+rmtrms@users.noreply.github.com>
2025-11-21 09:14:50 +0100

    fix: wrong security policy path in tests

--- tests/scripts/cases/advise/advise_enrich-inventory-02.sh
+++ tests/scripts/cases/advise/advise_enrich-inventory-02.sh
@@ -3,7 +3,7 @@
 export INPUT_INVENTORY_FILE="$ADVISED_DIR_002/busybox-1.35.0-enriched-with-reference.xlsx"
 export OUTPUT_INVENTORY_FILE="$ADVISED_DIR_002/busybox-1.35.0-enriched.xlsx"
 export VULNERABILITY_MIRROR_DIR="$EXTERNAL_VULNERABILITY_MIRROR_DIR/.database"
-export SECURITY_POLICY="$EXTERNAL_WORKBENCH_DIR/policies/security-policy.json"
+export SECURITY_POLICY="$EXTERNAL_WORKBENCH_DIR/policies/security-policy/security-policy.json"
 export PROCESSOR_TMP_DIR="$ADVISED_DIR_002/tmp"
 export CONTEXT_DIR="$EXTERNAL_WORKBENCH_DIR/contexts"
 export ASSESSMENT_DIR="$EXTERNAL_WORKBENCH_DIR/assessments"
