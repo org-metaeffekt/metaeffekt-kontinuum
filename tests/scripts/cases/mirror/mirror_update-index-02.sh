628d1cd63e20253fad585cf38b9d54a920fac837
jonasfuegen <jonas.fuegen@metaeffekt.com>
2025-12-01 09:45:16 +0100

    AE-1531-refactor-pipeline-scripts
    - restored update-index-02 case required to build external mirrors

---
+++ tests/scripts/cases/mirror/mirror_update-index-02.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Supports a reduced index of selected data sources for distribution
+
+export ENV_MIRROR_DIR="$EXTERNAL_VULNERABILITY_MIRROR_DIR"
+export PROCESSOR_POM="mirror/mirror_update-index_external.xml"
