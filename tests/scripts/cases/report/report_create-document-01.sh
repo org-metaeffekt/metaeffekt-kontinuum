8f0c58a3e0e392ce0b8667981d0f07cd49f660de
rmtrms <91293797+rmtrms@users.noreply.github.com>
2025-12-23 09:11:11 +0100

    AE-1678: updated test cases for absolute asset-descriptor paths

--- tests/scripts/cases/report/report_create-document-01.sh
+++ tests/scripts/cases/report/report_create-document-01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export INPUT_ASSET_DESCRIPTOR_PATH="asset-descriptor_GENERIC-vulnerability-report.yaml"
 export INPUT_INVENTORY_FILE="$AGGREGATED_DIR_001/sample-asset-1.0/sample-asset-1.0-inventory.xls"
 export OUTPUT_DOCUMENT_FILE="$REPORTED_DIR_001/asset-vulnerability-report.pdf"
 export OUTPUT_COMPUTED_INVENTORY_DIR="$REPORTED_DIR_001/computed"
+export PARAM_ASSET_DESCRIPTOR_FILE="$EXTERNAL_WORKBENCH_DIR/descriptors/asset-descriptor_GENERIC-vulnerability-report.yaml"
 export PARAM_REFERENCE_INVENTORY_DIR="$EXTERNAL_WORKBENCH_DIR/inventories/example-reference-inventory/inventory"
 export PARAM_SECURITY_POLICY_FILE="$EXTERNAL_WORKBENCH_DIR/policies/security-policy/security-policy.json"
 export PARAM_DOCUMENT_TYPE="VR"
