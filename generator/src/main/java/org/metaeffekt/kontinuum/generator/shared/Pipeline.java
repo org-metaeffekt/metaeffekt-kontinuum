4a7854a7650305bb387ec12ccbac2b58831ecf40
jonasfuegen <jonas.fuegen@protonmail.com>
2026-07-07 14:01:56 +0200

    fixed portfolio download unzip logic

--- generator/src/main/java/org/metaeffekt/kontinuum/generator/shared/Pipeline.java
+++ generator/src/main/java/org/metaeffekt/kontinuum/generator/shared/Pipeline.java
@@ -344,7 +344,7 @@ public class Pipeline {
         StringBuilder postScript =  new StringBuilder();
         postScript.append("find ").append(workspace.getStageDirForAsset(asset, Stage.AGGREGATE).toString()).append(" -type f -name \"*.zip\" -print0 | while IFS= read -r -d '' zip_file; do").append(System.lineSeparator());
         postScript.append("    zip_dir=$(dirname \"$zip_file\")").append(System.lineSeparator());
-        postScript.append("    unzip -q -j \"$zip_file\" \"*.xls\" \"*.xlsx\" -d \"$zip_dir\"").append(System.lineSeparator());
+        postScript.append("    unzip -q -j \"$zip_file\" \"*.xls\" \"*.xlsx\" -d \"$zip_dir\" || true").append(System.lineSeparator());
         postScript.append("done").append(System.lineSeparator());
 
         processor.setPostScript(postScript.toString());
