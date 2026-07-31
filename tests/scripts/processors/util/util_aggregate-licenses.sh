3b173e3caffe53f35d04118a456364e012a04420
romeotueremis <romeo.tueremis@metaeffekt.com>
2026-07-31 13:39:43 +0200

    fixed pipeline

--- tests/scripts/processors/util/util_aggregate-licenses.sh
+++ tests/scripts/processors/util/util_aggregate-licenses.sh
@@ -40,7 +40,7 @@ initialize_logger() {
 
 #Run maven command
 run_maven_command() {
-  CMD=(mvn -f "$PROCESSORS_DIR/report/util_aggregate-licenses.xml" verify)
+  CMD=(mvn -f "$PROCESSORS_DIR/util/util_aggregate-licenses.xml" verify)
   [ "${DEBUG:-}" = "true" ] && CMD+=("-X")
   [ -n "${AE_CORE_VERSION:-}" ] && CMD+=("-Dae.core.version=$AE_CORE_VERSION")
   [ -n "${AE_ARTIFACT_ANALYSIS_VERSION:-}" ] && CMD+=("-Dae.artifact.analysis.version=$AE_ARTIFACT_ANALYSIS_VERSION")
