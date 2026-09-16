b0fcf2710bb1ea80e0a5f7990aaf6c609d87782e
jonasfuegen <jonas.fuegen@protonmail.com>
2026-09-16 14:12:42 +0200

    fixed protocol.log being written to test resources

--- tests/scripts/cases/util/util_aggregate-sources-01.sh
+++ tests/scripts/cases/util/util_aggregate-sources-01.sh
@@ -3,4 +3,4 @@
 export INPUT_INVENTORY_FILE="$WORKSPACE_001_DIR/sample-product-1.0.0/01_assets/example-001.xlsx"
 export OUTPUT_TARGET_DIR="$AGGREGATED_DIR_001"
 export PARAM_CONFIG_FILE="$EXTERNAL_WORKBENCH_DIR/config/source-aggregation/config.yaml"
-export PARAM_PROTOCOL_FILE="$WORKSPACE_001_DIR/sample-product-1.0.0/01_assets/protocol.log"
+export PARAM_PROTOCOL_FILE="$TARGET_WORKSPACE_001_DIR/sample-product-1.0.0/01_assets/protocol.log"
